a5240af819234f8b861f7933f7fe4619332e2c0c
Simon Olander <simon.olander@r2m.se>
2020-04-11 10:04:28 +0200

    Added acl permissions

--- setup-aws.sh
+++ setup-aws.sh
@@ -69,8 +69,10 @@ policy_json=$(aws iam create-policy "--policy-name=${policy_name}" --policy-docu
         "s3:DeleteObject",
         "s3:GetBucketLocation",
         "s3:GetObject",
+        "s3:GetObjectAcl",
         "s3:ListBucket",
-        "s3:PutObject"
+        "s3:PutObject",
+        "s3:PutObjectAcl"
       ],
       "Resource": [
         "arn:aws:s3:::'"${bucket_name}"'",
